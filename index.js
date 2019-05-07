36e2e94c377ebb5f6ffd80d36b1939bb2b86b5ea
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-05-07 08:00:37 -0300

    scalable version

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ appDiv.innerHTML = `<h1>JS Starter Quiz</h1>`;
 let questionsTemplate = ``;
 const questionsContainer = document.getElementById('questionsContainer');
 const questionObjects = [];
-const wrongMessage = `<p class="wrong"><b>It is not right!</b>Try again!<\p>`;
 
 for (let questionData of QUESTIONS ) {
   let question = new Question( questionData );
@@ -32,14 +31,16 @@ for (let questionData of QUESTIONS ) {
   `
   questionsContainer.innerHTML += questionsTemplate;
 }
+//Add event listners
 for(let questionobject of questionObjects){
   document.getElementById(questionobject.questionId).addEventListener('click', checkAnswer);
 }
 
 
-
+//check answers
 function checkAnswer( $event ) {
-  let clickedObject = questionObjects.filter( ( obj ) => { return obj.questionId === Number($event.target.id) } )[0]
+  let clickedObject = questionObjects.filter( ( obj ) => { return obj.questionId === Number($event.target.id) } )[0];
+  const wrongMessage = `<p class="wrong"><b>It is not right!</b>Try again!<\p>`;
   console.log(clickedObject);
   console.log(clickedObject.getCorrectAnswer());  
   const answers = document.getElementsByName('answer' + clickedObject.questionId.toString() );
@@ -54,52 +55,7 @@ function checkAnswer( $event ) {
     }
   }
 }
-/*
-//Question 1
-btn1.onclick = function() {
-  const answers = document.getElementsByName('answer1');
-  const answer1 = document.getElementById('answer1');
-  if (answers[0].checked){
-   answer1.innerHTML = `<p class="correct">You are right! JavaScript is a High Level language, so it can not be a low level language!<\p>`
-  }
-  else {
-    answer1.innerHTML = wrongMessage;
-    for( let answer of answers ) {
-      answer.checked = false;
-    }
-  }
-}
 
-//Question 2
-btn2.onclick = function() {
-  const answers = document.getElementsByName('answer2');
-  const answer2 = document.getElementById('answer2');
-  if (answers[1].checked){
-   answer2.innerHTML = `<p class="correct">You are right! A function is answer1 chunk of code that is associated to a name!<\p>`
-  }
-  else {
-    answer2.innerHTML = wrongMessage;
-    for( let answer of answers ) {
-      answer.checked = false;
-    }
-  }
-}
-
-//Question 3
-btn3.onclick = function() {
-  const answers = document.getElementsByName('answer3');
-  const answer3 = document.getElementById('answer3');
-  if (answers[2].checked){
-   answer3.innerHTML = `<p class="correct">You are right! Arrays are objects with numerical indexes<\p>`
-  }
-  else {
-    answer3.innerHTML = wrongMessage;
-    for( let answer of answers ) {
-      answer.checked = false;
-    }
-  }
-}
-*/
 
 
 
