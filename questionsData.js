0b6b538c6f78bfed7120e4290f39d4f939833f00
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-05-06 22:46:14 -0300

    questionContainers

--- questionsData.js
+++ questionsData.js
@@ -1,7 +1,27 @@
 //ES6 like file
 export const QUESTIONS = [
   [1, 
-  'blá, blé, blí, bló, blú',
-  [{text: 'a. opção a', isCorrect: true}, {text: 'b. opção b', isCorrect: false}, {text: 'c. opção c', isCorrect: false}]
+  'What is NOT a JavaScript characteristic?',
+    [
+      {text: 'a. is a low level language.', isCorrect: true},
+      {text: 'b. is a high level language.', isCorrect: false},
+      {text: 'c. is an intrpreted language typed', isCorrect: false}
+    ]
+  ],
+  [2,
+    'What is a JavaScript function?',
+    [
+      {text: 'a. JavaScript does not support functions.', isCorrect: false},
+      {text: 'b. A chunk of code that is associated to a name.', isCorrect: true},
+      {text: 'c. Codes that execute a mathematical function.', isCorrect: false}
+    ]
+  ],
+  [3, 
+    'What is an array?',
+    [
+      {text: 'a. An array is an object with no indexes.', isCorrect: false},
+      {text: 'b. An array is a set of numbers.', isCorrect: false},
+      {text: 'c. An array is an object organized by numerical indexes.', isCorrect: true}      
+    ]
   ]
 ];
